d8c23286ce227327eab8598671bae391ae0fb9d5
Tsalikis Stavros <stsal1992@gmail.com>
2020-08-06 23:00:06 +0300

    Add files via upload

---
+++ TcpServerImplement/src/Messaging.java
@@ -0,0 +1,30 @@
+
+/**
+ * @author stavros
+ *
+ */
+public class Messaging {
+	private String sender;
+	private String message;
+	private String subject;
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+}
